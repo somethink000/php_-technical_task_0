f4f47d356226106e5139a60306b69c297cc9aa9f
some <zubkoffalex4nder@yandex.ru>
2024-05-06 13:24:29 +0300

    done

--- web/DBManager.php
+++ web/DBManager.php
@@ -12,30 +12,75 @@ function conectionEnd($value){
   mysqli_close($value);
  }
 function fetch($value=''){
-  $connection = conectionStart();
-  $sql = "SELECT * FROM Posts WHERE title LIKE '%".$value."%'";
-  $result = mysqli_query($connection, $sql);
+    $connection = conectionStart();
+    
+    $result = array();
 
-  if($result) {
-    echo "<table>";
-    echo "<tr>";
-    echo "<th>Id</th>";
-    echo "<th>Name</th>";
-    echo "</tr>";
-    while ($row = mysqlI_fetch_assoc($result)) {
-      echo "<tr>";
-      echo "<td>".$row['id']."</td>";
-      echo "<td>".$row['title']."</td>";
-      echo "</tr>";
-      }
-      echo "</table>";
-    } else {
-      echo "Error :".$sql."<br>".mysqli_error($connection);
+    if (strlen($value) > 2)
+    {
+
+        $commentsResult = mysqli_query($connection, "SELECT * FROM Comments WHERE name LIKE '%".$value."%'");
+
+        
+        foreach ($commentsResult as $item)
+        {   
+            
+            $postsResult = mysqli_query($connection, "SELECT * FROM Posts WHERE id = '".$item['postId']."'"); 
+            
+            foreach($postsResult as $i)
+            {
+                
+                if(!isset($result[$i['id']]))
+                {
+                    $result[$i['id']] = $i;
+                    $result[$i['id']]['comment'] = $item['name'];
+                }
+            }
+            
+        }
+    }else
+    {
+        $result = mysqli_query($connection, "SELECT * FROM Posts"); 
     }
 
-    conectionEnd($connection);
-  }
-if (isset($_GET['search'])) {
-  fetch($_GET['search']);
-  }
+
+    if($result) {
+        
+
+        echo "<thead>";
+            echo "<tr>";
+                echo "<th scope='col'>Id</th>";
+                echo "<th scope='col'>Title</th>";
+                echo "<th scope='col'>Body</th>";
+                echo "<th scope='col'>Comment</th>";
+            echo "</tr>";
+        echo "</thead>";
+        echo "<tbody>";
+        
+        foreach($result as $row){
+            
+            echo "<tr>";
+            echo "<th scope='row'>".$row['id']."</th>";
+            echo "<td>".$row['title']."</td>";
+            echo "<td>".$row['body']."</td>";
+
+            if (isset($row['comment'])){
+                echo "<td>".$row['comment']."</td>";
+            }
+            
+            echo "</tr>";
+            
+        }
+
+        echo "</tbody>";
+
+        } else {
+            return;
+        }
+
+        conectionEnd($connection);
+    }
+    if (isset($_GET['search'])) {
+        fetch($_GET['search']);
+    }
 ?>
--- web/index.php
+++ web/index.php
@@ -1,52 +1,3 @@
-
-<html lang="en" dir="ltr">
-  <head>
-    <title>MIM</title>
-    <script>
-      function usersearchTxt(str) {
-        if (str.length == 0) {
-          document.getElementById("searchTable").innerHTML = "";
-          return;
-        } else {
-          var xmlhttp = new XMLHttpRequest();
-          xmlhttp.onreadystatechange = function () {
-            if(this.readyState == 4 && this.status == 200){
-              document.getElementById('searchTable').innerHTML = this.responseText;
-              //document.getElementById("searchTable").style.border="1px solid #A5ACB2";
-              }
-            }
-          xmlhttp.open("GET","DBManager.php?search="+str,true);
-          xmlhttp.send();
-
-        }
-      }
-    </script>
-  </head>
-  <body>
-    <input id="searchBox" type="text" name="" onkeyup="usersearchTxt(this.value)">
-    <button type="button" name="button" onclick="usersearchTxt(document.getElementById('searchBox').value);">search</button>
-
-    <div id="searchTable">
-      <table>
-        <tr>
-          <th>Id</th>
-          <th>Name</th>
-        </tr>
-        <?php
-          include 'DBManager.php';
-          echo fetch('');
-        ?>
-      </table>
-    </div>
-  </body>
-</html>
-
-
-
-
-
-
-<!-- 
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -60,16 +11,36 @@
 
         <title></title>
         
+        <script>
+            function usersearchTxt(str) {
+                if (str.length == 0) {
+                document.getElementById("searchTable").innerHTML = "";
+                return;
+                } else {
+                var xmlhttp = new XMLHttpRequest();
+                xmlhttp.onreadystatechange = function () {
+                    if(this.readyState == 4 && this.status == 200){
+                    document.getElementById('searchTable').innerHTML = this.responseText;
+                    //document.getElementById("searchTable").style.border="1px solid #A5ACB2";
+                    }
+                    }
+                xmlhttp.open("GET","DBManager.php?search="+str,true);
+                xmlhttp.send();
+
+                }
+            }
+        </script>
        
     </head>
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container px-4 px-lg-5">
-            <a class="navbar-brand" href="/">Import in mysql example</a>
+            <a class="navbar-brand" href="/">Example</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-        
+                   
+                   
                 </ul>
 
             </div>
@@ -77,53 +48,30 @@
     </nav>
     
 
-    <body class="container">
+    <body>
+        <div class="container">
+            
 
-        <?php
-
-    
-            $con = mysqli_connect("db","example","secret2","stage") or die('Failed to connect to the database, died with error:');
-            $sql = "SELECT * FROM Posts";
-            $result = mysqli_query($con, $sql);
-
-
-            if ($result->num_rows > 0) {
-
-                while($row = $result->fetch_assoc()) {
-                    echo "id: " . $row["id"]. " - Name: " . $row["title"]. " " . $row["body"]. "<br>";
-                }
-            } else {
-                echo "0 results";
-            }
-
-        ?>
-
-
-            <br/>
-            <div class="row">
-                <div class="col-sm-8">
-                        <form  onsubmit="submit_data(null);return false;" method="post">
-                        <input type="text" autocomplete="off" class="form-control" id="pharse" placeholder="enter your search phrase here..."/>
-                </div>
-                <div class="col-sm-2">
-                        <input type="submit" name="submit" class="btn btn-primary"/>
-                        </form>
-                </div>
-                <div class="col-sm-2">
-                    <img id="loadingDiv" src="public/img/ajax-loader.gif" />
-                </div>
+            <div class="form-outline mb-4 py-5" data-mdb-input-init>
+                <input placeholder="Search" type="search" class="form-control" id="searchBox" onkeyup="usersearchTxt(this.value)">
             </div>
-            </div>
-            <div>
-                <ul class="list-group floatinglist" id="searchKeyLists">
-                </ul>
 
             
+
+            <table class="table table-hover" id="searchTable">
+                
+                    
+                    <?php
+                        include 'DBManager.php';
+                        echo fetch('');
+                    ?>
+                
+            </table>
+            
+        </div>
     </body>
 
 
     <footer class="py-2 bg-dark"> </footer>
 
-</html>  
-   
- -->
+</html>
